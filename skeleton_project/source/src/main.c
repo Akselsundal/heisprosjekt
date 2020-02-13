d87e6977363a7d321d4cf7b97aee291f282e6fb4
sofiet <sofiet@stud.ntnu.no>
2020-02-13 16:02:14 +0100

    Fikset mappestruktur og makefile

--- skeleton_project/source/src/main.c
+++ skeleton_project/source/src/main.c
@@ -1,46 +1,38 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <signal.h>
 #include "hardware.h"
-#include "elevator.h"
 
-void parse_argvs(char argc, char **argv);
-void run_example_program();
+static void clear_all_order_lights(){
+    HardwareOrder order_types[3] = {
+        HARDWARE_ORDER_UP,
+        HARDWARE_ORDER_INSIDE,
+        HARDWARE_ORDER_DOWN
+    };
 
-int main(int argc, char **argv){
-
-    parse_argvs(argc, argv);
-
-
-
+    for(int f = 0; f < HARDWARE_NUMBER_OF_FLOORS; f++){
+        for(int i = 0; i < 3; i++){
+            HardwareOrder type = order_types[i];
+            hardware_command_order_light(f, type, 0);
+        }
+    }
 }
 
-
-
-
-
-
-
-
-
-
-void parse_argvs(char argc, char **argv){
-    if (!strcmp(argv[1], "example")){
-        run_example_program();
-    }
-    if (!strcmp(argv[1], "run")){
-        run_elevator();
-    }
-
-
+static void sigint_handler(int sig){
+    (void)(sig);
+    printf("Terminating elevator\n");
+    hardware_command_movement(HARDWARE_MOVEMENT_STOP);
+    exit(0);
 }
-void run_example_program(){
-    // Example program
+
+int main(){
     int error = hardware_init();
     if(error != 0){
         fprintf(stderr, "Unable to initialize hardware\n");
         exit(1);
     }
+
+    signal(SIGINT, sigint_handler);
 
     printf("=== Example Program ===\n");
     printf("Press the stop button on the elevator panel to exit\n");
@@ -59,5 +51,40 @@ void run_example_program(){
         if(hardware_read_floor_sensor(HARDWARE_NUMBER_OF_FLOORS - 1)){
             hardware_command_movement(HARDWARE_MOVEMENT_DOWN);
         }
+
+        /* All buttons must be polled, like this: */
+        for(int f = 0; f < HARDWARE_NUMBER_OF_FLOORS; f++){
+            if(hardware_read_order(f, HARDWARE_ORDER_INSIDE)){
+                hardware_command_floor_indicator_on(f);
+            }
+        }
+
+        /* Lights are set and cleared like this: */
+        for(int f = 0; f < HARDWARE_NUMBER_OF_FLOORS; f++){
+            /* Internal orders */
+            if(hardware_read_order(f, HARDWARE_ORDER_INSIDE)){
+                hardware_command_order_light(f, HARDWARE_ORDER_INSIDE, 1);
+            }
+
+            /* Orders going up */
+            if(hardware_read_order(f, HARDWARE_ORDER_UP)){
+                hardware_command_order_light(f, HARDWARE_ORDER_UP, 1);
+            }
+
+            /* Orders going down */
+            if(hardware_read_order(f, HARDWARE_ORDER_DOWN)){
+                hardware_command_order_light(f, HARDWARE_ORDER_DOWN, 1);
+            }
+        }
+
+        if(hardware_read_obstruction_signal()){
+            hardware_command_stop_light(1);
+            clear_all_order_lights();
+        }
+        else{
+            hardware_command_stop_light(0);
+        }
     }
+
+    return 0;
 }
