f1ba54884afcce6b7d15aa7a021ad32debe2f307
Magnus Mæhlum <magnusmaehlum@outlook.com>
2020-02-13 14:33:48 +0100

    Lagde example program som alternativ fra main.

---
+++ skeleton_project/source/elevator.h
@@ -0,0 +1,7 @@
+#ifndef ELEVATOR_H
+#define ELEVATOR_H
+
+void run_example_program();
+
+
+#endif // ELEVATOR_H
--- skeleton_project/source/main.c
+++ skeleton_project/source/main.c
@@ -1,30 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "hardware.h"
+#include "elevator.h"
 
-int main(){
-    int error = hardware_init();
-    if(error != 0){
-        fprintf(stderr, "Unable to initialize hardware\n");
-        exit(1);
+void parse_argvs(char argc, char **argv);
+
+int main(char argc, char **argv){
+
+    parse_argvs(argc, argv);
+
+
+
+}
+
+void parse_argvs(char argc, char **argv){
+    if (strcmp(argv[1]), "example"){
+        run_example_program();
     }
 
-    printf("=== Example Program ===\n");
-    printf("Press the stop button on the elevator panel to exit\n");
 
-    hardware_command_movement(HARDWARE_MOVEMENT_UP);
-
-    while(1){
-        if(hardware_read_stop_signal()){
-            hardware_command_movement(HARDWARE_MOVEMENT_STOP);
-            break;
-        }
-
-        if(hardware_read_floor_sensor(0)){
-            hardware_command_movement(HARDWARE_MOVEMENT_UP);
-        }
-        if(hardware_read_floor_sensor(HARDWARE_NUMBER_OF_FLOORS - 1)){
-            hardware_command_movement(HARDWARE_MOVEMENT_DOWN);
-        }
-    }
 }
